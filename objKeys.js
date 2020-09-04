a9c839039dd77a7b24216592e3d2d83498e449bf
Savi Williams <savannah.williams1623@gmail.com>
2020-09-04 10:56:05 -0600

    works but is not pretty

---
+++ objKeys.js
@@ -0,0 +1,24 @@
+// Write a JavaScript program to list the properties of a JavaScript object.
+// Sample object:
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+// Sample Output: name,sclass,rollno
+
+const objKeys = function (obj) {
+  let string = '';
+
+  for (let key in obj) {
+    string += `${key}, `;
+  }
+  return string;
+};
+
+var student = {
+  name: 'David Rayy',
+  sclass: 'VI',
+  rollno: 12,
+};
+
+console.log(objKeys(student));
